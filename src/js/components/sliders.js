09eb28a68c1683cf3e386b1c225dc7882bc32a3b
trobiuk <trobiuk@gmail.com>
2022-02-15 20:02:52 +0200

    add top slider on home page

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -90,3 +90,35 @@ if (historySlider) {
     });
   });
 }
+
+const heroSliderSpeed = 1500;
+
+const bodyStyles = window.getComputedStyle(document.body);
+const fooBar = bodyStyles.getPropertyValue('--hero-slider-speed'); //get
+document.body.style.setProperty('--hero-slider-speed', heroSliderSpeed + 'ms');//set
+
+const heroSlider = new Swiper('.hero-slider', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.hero__next',
+    prevEl: '.hero__prev',
+  },
+  speed: heroSliderSpeed,
+  autoplay: {
+    delay: 1000,
+  },
+  pagination: {
+    el: '.hero__pag',
+    type: 'bullets',
+    clickable: true
+  },
+  on: {
+      init: function () {
+        const paginationBullets = document.querySelectorAll('.hero__pag .swiper-pagination-bullet');
+
+        paginationBullets.forEach(el => {
+          el.innerHTML = `<span class="hero__bar"></span>`;
+        });
+      },
+    },
+});
