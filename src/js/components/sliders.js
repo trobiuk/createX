3f99a6a4c38ae4e4fe86218fd33300ec42b66918
trobiuk <trobiuk@gmail.com>
2022-05-18 17:16:51 +0300

    add mobile for worck-1 page

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -49,8 +49,17 @@ const workImages = document.querySelector(".work-images-slider");
 if (workImages) {
   const workSlider = new Swiper(".work-images-nav", {
     spaceBetween: 20,
-    slidesPerView: 10,
+    slidesPerView: 3,
     freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      576: {
+        slidesPerView: 6
+      },
+      768: {
+        slidesPerView: 10,
+      },
+    }
   });
 
   const workSlidesNav = new Swiper(workImages, {
